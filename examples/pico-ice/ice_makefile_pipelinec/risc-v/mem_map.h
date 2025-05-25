54f87a16f5faaf03d420681594991c7e13dd3c1d
Edward Vidal Jr <develone@sbcglobal.net>
2025-05-24 18:55:05 -0600

    wkg

--- examples/pico-ice/ice_makefile_pipelinec/risc-v/mem_map.h
+++ examples/pico-ice/ice_makefile_pipelinec/risc-v/mem_map.h
@@ -13,7 +13,7 @@
 
 // Define bounds for IMEM, DMEM, and MMIO
 // Needs to match link.ld (TODO how to share variables?)
-#define IMEM_SIZE 512 // Must be decimal constant since VHDL+C literal
+#define IMEM_SIZE 2048 // Must be decimal constant since VHDL+C literal
 #define DMEM_SIZE 2048 // Must be decimal constant since VHDL+C literal
 #define DMEM_ADDR_BIT_CHECK 30
 #define DMEM_BASE_ADDR ((uint32_t)((uint32_t)1<<DMEM_ADDR_BIT_CHECK))
